34a48d7fa341d21e189902b576afab8634413a15
MartijnScale <36644394+MartijnScale@users.noreply.github.com>
2022-01-31 11:24:01 +0100

    Update test.ts
    Simple test added

--- test.ts
+++ test.ts
@@ -1,1 +1,6 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+CCS811.CCS811_Init()
+basic.forever(function () {
+    basic.showNumber(CCS811.GetAlgResulteCO2())
+    basic.pause(1000)
+})
